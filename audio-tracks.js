8e989289f916fba640aeb044d5f8afea00f6433b
hohm <therealz@protonmail.com>
2024-12-06 08:55:40 -0800

    Implement A/B version switching for meditation tracks
    - Added separate directories for version A and B tracks
    - Updated audio file paths in audio-tracks.js
    - Fixed AudioContext initialization to require user gesture
    - Updated README with new directory structure
    - Added audio directory structure

--- audio-tracks.js
+++ audio-tracks.js
@@ -1,52 +1,55 @@
 const audioTracks = {
     'Dreams II': {
         name: 'Sacred Solfeggio Dreams II',
-        versionA: 'https://suno.com/song/ced723f8-78ef-4698-924f-e5da637cf285',
-        versionB: 'https://suno.com/song/624a9e33-9f17-4072-8466-e3b758ec0aaa'
-    },
+        versionA: 'audio/tracks/version-a/Sacred Solfeggio Dreams II-2.mp3',
+        versionB: 'audio/tracks/version-b/Sacred Solfeggio Dreams II.mp3'
+    }
+    // Commenting out other tracks until they are added
+    /*,
     'Sacred Solfeggio': {
         name: 'Sacred Solfeggio Dreams',
-        versionA: 'https://suno.com/song/e5fb7cd4-d001-491f-8be2-87c81a611ef3',
-        versionB: 'https://suno.com/song/bce30391-0f4f-4659-a962-2fc1be0107d5'
+        versionA: 'audio/tracks/version-a/Sacred Solfeggio Dreams.mp3',
+        versionB: 'audio/tracks/version-b/Sacred Solfeggio Dreams.mp3'
     },
     'Angels Breath': {
         name: "Angel's Breath",
-        versionA: 'https://suno.com/song/0fbff4d8-9608-4ad2-a850-08a97511f14c',
-        versionB: 'https://suno.com/song/59767eab-9f53-4fa8-9e13-26029c69fced'
+        versionA: 'audio/tracks/version-a/Angels Breath.mp3',
+        versionB: 'audio/tracks/version-b/Angels Breath.mp3'
     },
     'Mother Earth': {
         name: "Mother Earth's Embrace",
-        versionA: 'https://suno.com/song/e9ab2718-c8a0-420e-b23b-f11deb7f80e6',
-        versionB: 'https://suno.com/song/e8a602b8-4cea-4cef-b8f7-7beab290d54b'
+        versionA: 'audio/tracks/version-a/Mother Earths Embrace.mp3',
+        versionB: 'audio/tracks/version-b/Mother Earths Embrace.mp3'
     },
     'Sacred Temple': {
         name: 'Sacred Temple Gardens',
-        versionA: 'https://suno.com/song/5ebb2ef2-341d-4739-9789-205e2a7f2c2e',
-        versionB: 'https://suno.com/song/eec72d38-f1c1-4695-ab00-d4cad417141f'
+        versionA: 'audio/tracks/version-a/Sacred Temple Gardens.mp3',
+        versionB: 'audio/tracks/version-b/Sacred Temple Gardens.mp3'
     },
     'Forest Night': {
         name: 'Forest Night',
-        versionA: 'https://suno.com/song/cf64b401-b204-4ad6-bbb7-a55388c0ca8a',
-        versionB: 'https://suno.com/song/2fd80035-1dda-4b57-8b97-1ecc6ece25de'
+        versionA: 'audio/tracks/version-a/Forest Night.mp3',
+        versionB: 'audio/tracks/version-b/Forest Night.mp3'
     },
     'Deep Ocean': {
         name: 'Deep Ocean',
-        versionA: 'https://suno.com/song/bc15545c-7142-4ca7-81fa-fb3ba497b16b',
-        versionB: 'https://suno.com/song/a3f41cec-9341-4f40-82da-4c3de3a10278'
+        versionA: 'audio/tracks/version-a/Deep Ocean.mp3',
+        versionB: 'audio/tracks/version-b/Deep Ocean.mp3'
     },
     'Cosmic Flow': {
         name: 'Cosmic Flow',
-        versionA: 'https://suno.com/song/786cb769-2b50-4461-a2ae-42f893f22627',
-        versionB: 'https://suno.com/song/b38f43b2-c69c-4f86-b5f3-0ce62f06f6a7'
+        versionA: 'audio/tracks/version-a/Cosmic Flow.mp3',
+        versionB: 'audio/tracks/version-b/Cosmic Flow.mp3'
     },
     'Deepest Surrender': {
         name: 'Deepest Surrender',
-        versionA: 'https://suno.com/song/8021d88a-4811-4684-8cfa-a02f7fb17bfc',
-        versionB: 'https://suno.com/song/fa9ab3cd-688a-4579-9a81-42681fdbcc59'
+        versionA: 'audio/tracks/version-a/Deepest Surrender.mp3',
+        versionB: 'audio/tracks/version-b/Deepest Surrender.mp3'
     },
     'Coming Home': {
         name: 'Coming Home',
-        versionA: 'https://suno.com/song/d57fa1bb-0ce5-433c-b005-8cc37aabdaff',
-        versionB: 'https://suno.com/song/8d5ded3d-6655-4892-9740-76c695b89cc5'
+        versionA: 'audio/tracks/version-a/Coming Home.mp3',
+        versionB: 'audio/tracks/version-b/Coming Home.mp3'
     }
+    */
 };
